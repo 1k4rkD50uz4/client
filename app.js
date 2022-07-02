1cd9d44136106d3bbcd845b27c18b430eeb4357f
Mark Dsouza <mark.francis.dsouza@gmail.com>
2022-07-02 16:20:00 -0700

    todo

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
 import net from 'net';
-const s = "The quick brown fox jumped over the lazy dog";
-let iter = s[Symbol.iterator](),
-    res = iter.next(),
-    value = res.value,
-    remotePort = 8000,
-    outData = { value: null };
-const client = net.createConnection({ port: remotePort }, () => {
-        console.log('connected to server!');
-    });
-    client.on('data', (data) => {
-        console.log(`char from server: ${data.toString()}`);
-        setNextChar();
-        client.write(outData.value);
-        client.end();
-    });
-    client.on('end', () => {
-        console.log('disconnected from server');
-});
-function setNextChar() {
-    res = iter.next();
-    if (compare(res.value, value)) {
-        outData.value = res.value;
-    }
+//import StringUtils from '../lib/stringUtils.js';
+import encodeNumber from '../lib/number.js';
+import { Buffer } from 'buffer';
+//const stringUtils = StringUtils(),
+//    buffer;
+//const client = net.createConnection({ port: stringUtils.port }, () => {
+//        console.log('connected to server!');
+//    });
+//    client.on('data', (data) => {
+//        console.log(`char from server: ${data.toString()}`);
+//        client.write(i);
+//        client.end();
+//    });
+//    client.on('end', () => {
+//        console.log('disconnected from server');
+//});
+let i = parseInt(process.argv[2]),
+    c = process.argv[3],
+    res;
+function main(args) {
+    const props = {
+        value: 0
+    },
+    methods = {
+        inc: i => ++i,
+        cb: inc,
+        args=[cb(i)],
+        itoa = () => {
+            let i = props.value,
+                exp = args.shift(),
+                cb = !cb ? parseInt : cb,
+                options = !options ? [cb(i)] : options.concat([options[i]]),
+                res;
+            for (let arg of args) {
+                exp += arg;
+            }
+            res = cb(exp, options[i]);
+            return res;
+            // 10
+            //64+1=A,32, 8 = 26
+        }
+    },
+    Exp = Object.create(val, methods);
+    encodeNumber(Exp.itoa());
+    let c = '0x0',
+        exp=Exp(cb())
 }
-function compare(a, b) {
-    if (a < b) {
-        return -1;
-    }
-    else if (a > b) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
+main(process.argv);
+const buffer = encodeString(c);
+res = buffer[5];
+process.stdout.write(res.toString());
