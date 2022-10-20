118c54e52e84a34acbbe1a292cb75fcb80abc0c9
Mark D'Souza <mark.francis.dsouza@gmail.com>
2022-10-20 11:56:39 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
-import net from 'net';
-const s = "The quick brown fox jumped over the lazy dog";
-let iter = s[Symbol.iterator](),
-    res = iter.next(),
-    value = res.value,
-    hostName = "127.0.0.1",
-    port = process.env.PORT || 8000,
-    outData = { value: null };
-const client = net.createConnection({
-    port: port,
-    host: hostName
-}, () => {
-        console.log('connected to server!');
-    });
-    client.on('data', (data) => {
-        console.log(`char from server: ${data.toString()}`);
-        setNextChar();
-        client.write(outData.value);
-        client.end();
-    });
-    client.on('end', () => {
-        console.log('disconnected from server');
+import net from 'node:net';
+const client = net.createConnection({ port: 8124 }, () => {
+    // 'connect' listener.
+    console.log('connected to server!');
+    client.write('world!\r\n');
 });
-function setNextChar() {
-    res = iter.next();
-    if (compare(res.value, value)) {
-        outData.value = res.value;
-    }
-}
-function compare(a, b) {
-    if (a < b) {
-        return -1;
-    }
-    else if (a > b) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
+client.on('data', (data) => {
+    console.log(data.toString());
+    client.end();
+});
+client.on('end', () => {
+    console.log('disconnected from server');
+});
