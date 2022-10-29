4a8d68537b4158b1ed032f4755c13f5b008d982a
Mark D'Souza <mark.francis.dsouza@gmail.com>
2022-10-28 17:29:00 -0700

    arrow hof shared with server

--- sort.js
+++ sort.js
@@ -5,122 +5,26 @@ const s = "The quick brown fox jumped over the lazy dog",
         let [_i, _farr] = [...arguments];
         String.fromCharCode(+`0x${_i}`)
     },
-    getCode = (c = s[i]) => c.charCodeAt(i),
-    atoi = fcns => {
-        let res;
-        res = res['charCodeAt'](i);
-        let res;
-        for (let name of fcns) {
-            if (name == 'charCodeAt') {
-                if (!res) {
-                    res = s[i];
-                }
-                else {
-
-                }
-            }
-
-            res = String.prototype.hasOwnProperty('toLowerCase');
-        }
-        return res;
-        for (let name of fcns) { 
-            if(name == )
-            if (!res) {
-                res=
-            }
-            else {
-
-            }
-            res = String.prototype.hasOwnProperty('toLowerCase');
-        }
-        return res;
-    };
-let temp = atoi([c => c.toLowerCase(), getCode, inc]);
-function sort() {
-    let j = s.length - inc(i),
-        iter = s[Symbol.iterator](),
-        res = iter.next(),
-        c = res.value,
-        arr = (() => {            
-            let _arr = [s[('' + c.charCodeAt(i))[inc(i)]]];
-            c = String.fromCharCode(+`0x${atoi('' + c.charCodeAt(i))}`);
-            _arr.push.apply(_arr, [c.toLowerCase(), res.value.toLowerCase()]);
-            c = String.fromCharCode(inc(res.value.toLowerCase().charCodeAt(i)));
-            _arr.push(c);
-            c = s[j];
-            res = iter.next();
-            return _arr;
-        })();
-    while (!res.done) {
-        let _res = compare(c, res.value);
-        if (_res) {
-            if (inc(c.charCodeAt(i)) == res.value.charCodeAt(i)) {
-                arr.unshift.apply(arr, [c, res.value]);
-                j--;
-                c = s[i];
-            }
+    arr=[],
+    atoi = (val = s[i], fname = 'charCodeAt', count=0) => {
+        val = val[fname](i);
+        if (fname != 'charCodeAt') {
+            return atoi(val,undefined,count);
         }
         else {
+            if (count == 1) {
+                return atoi(val, null, count);
+            }
+            else {
+                res = inc(res);
+            }
             
         }
-        res = iter.next();
-
-    }
-    function compare(a, b) {
-        if (a < b) {
-            return true;
-        }
-        else if (a > b) {
-            return false;
+        if (pre == '0x') {
+            res = pre + res;
         }
         else {
-            return undefined;
+            res = +res;
         }
-    }
-    function getCode () {
-        let c = s[i],
-            arr = this.arr;
-        for (let f of arr) {
-            c = f(c);
-        }
-    }
-}
-sort();
-function init() {
-    outData.value = res.value;
-    res = enumtor();
-    arr.splice(i, i, s[j--], res.value);
-    res = enumtor();
-    c = s[j--];
-    arr.splice(i, i, s[j], res.value);
-}
+    };
 
-async function main() {
-    let _res;
-    while (!res.done) {
-        // Wait for data from the server
-        await timer(3000);
-        _res = compare(res.value, inData.value);
-        if (_res) {
-            if (inc(number) == next) {
-                max = s[j--];
-                min = s[j];
-            }
-            else {
-
-            }
-        }
-        else if (_res == false) {
-
-        }
-        //if (inc(number) == next) {
-        //    max = s[j--];
-        //    min = s[j];
-        //}
-        //else {
-
-        //}
-        res = enumtor();
-    }
-}
-//main();
