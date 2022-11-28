40defc6fa87cdf992699c44a04bfa8baa8f6f0b4
Mark D'Souza <mark.francis.dsouza@gmail.com>
2022-11-27 17:18:38 -0800

    added host parameter to specify server address in connection option object

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import net from 'node:net';
-const client = net.createConnection({ port: 8124 }, () => {
+import net from 'net';
+const client = net.createConnection({ host: "127.0.0.1", port: 8000 }, () => {
     // 'connect' listener.
     console.log('connected to server!');
     client.write('world!\r\n');
